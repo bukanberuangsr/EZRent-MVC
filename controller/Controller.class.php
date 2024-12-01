45cd2eaae41eafd5594f45e7782adbe8adb51b21
Akhtar Hafiz P <akhfizpuli@gmail.com>
2024-12-01 17:08:27 +0700

    deleted login function

--- controller/Controller.class.php
+++ controller/Controller.class.php
@@ -1,7 +1,6 @@
 <?php
 class Controller
 {
-
     public function loadModel($modelName)
     {
         include_once "model/Model.class.php";
@@ -15,31 +14,5 @@ class Controller
             $$var = $value;
         }
         include_once "view/$viewName.php";
-    }
-
-    public function login()
-    {
-        session_start();
-        $userModel = $this->loadModel("users");
-        $error_message = '';
-
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $username = $_POST['username'];
-            $password = $_POST['password'];
-
-            $user = $userModel->findByUsername($username);
-
-            if ($user && password_verify($password, $user['userpass'])) {
-                $_SESSION['username'] = $user['username'];
-                $_SESSION['is_admin'] = $user['is_admin'];
-
-                $redirect = $user['is_admin'] ? "/view/admin/dashboard" : "/index.php";
-                header('Location' . $redirect);
-                exit();
-            } else {
-                $error_message = "Username or Password is Incorrect!";
-            }
-        }
-        this->loadView('login', ['error_message'=> $error_message]);
     }
 }
