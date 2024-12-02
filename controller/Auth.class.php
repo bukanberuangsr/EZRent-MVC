8fd32564cfb1417ba50ae8936608b36ace65a02b
Akhtar Hafiz P <akhfizpuli@gmail.com>
2024-12-02 16:26:43 +0700

    Name change

--- controller/Auth.class.php
+++ controller/Auth.class.php
@@ -2,6 +2,14 @@
 
 class Auth extends Controller
 {
+    public function index() {
+        $this->loadView('login');
+    }
+
+    public function register() {
+        $this->loadView('register');
+    }
+
     public function login()
     {
         session_start();
@@ -26,6 +34,26 @@ class Auth extends Controller
             }
         }
         this->loadView('login', ['error_message'=> $error_message]);
+    }
+
+    public function registerSubmit()
+    {
+        session_start();
+        $error_message = '';
+        $success_message = '';
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            $confirm_password = $_POST['confirm_password'];
+
+            if ($password !== $confirm_password) {
+                $error_message = 'Password did not match!';
+            } else {
+                $hashed_pass = password_hash($password, PASSWORD_DEFAULT);
+                
+            }
+        }
     }
 
     public function logout()
