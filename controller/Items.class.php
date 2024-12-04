df56e17ed4a3630fc41020eb5cdcd3ebf6fe53d1
Akhtar Hafiz P <akhfizpuli@gmail.com>
2024-12-04 16:07:05 +0700

    upload and update adjustment

--- controller/Items.class.php
+++ controller/Items.class.php
@@ -15,16 +15,20 @@ class Items extends Controller
 
     public function create_form()
     {
-        $this->loadView('insert_post');
+        $this->loadView('insert_item');
     }
 
 
     public function create_process()
     {
         $itemModel = $this->loadModel('ItemModel');
-        $title = addslashes($_POST['title']);
-        $content = addslashes($_POST['content']);
-        $itemModel->insert($title, $content);
+
+        $title = addslashes($_POST['name']);
+        $description = addslashes($_POST['description']);
+        $available = addslashes($_POST['available']);
+        $image = $this->handleImageUpload();
+
+        $itemModel->insert($title, $description, $available, $image);
         header('Location: ?c=Items');
         exit;
     }
@@ -50,8 +54,10 @@ class Items extends Controller
         $itemModel = $this->loadModel('ItemModel');
 
         $id = $_POST['id'];
-        $title = addslashes($_POST['title']);
-        $content = addslashes($_POST['content']); //agar karakter aneh2 bisa kebaca, menghindari sql injection
+        $title = addslashes($_POST['name']);
+        $description = addslashes($_POST['description']);
+        $available = addslashes($_POST['available']);
+        $image = $this->handleImageUpload(); //agar karakter aneh2 bisa kebaca, menghindari sql injection
 
         $itemModel->update($id, $title, $content);
         header('Location: ?c=Items');
@@ -66,5 +72,29 @@ class Items extends Controller
 
         // redirect to post list after delete
         header('location:?c=Items');
+    }
+
+    public function handleImageUpload()
+    {
+        if ($_FILES['image']['error'] == 0) {
+            $target_dir = "/uploads/"; // Folder to store uploaded images
+            $target_file = $target_dir . basename($_FILES['image']['name']);
+            $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+            $allowed_types = ['jpg', 'jpeg', 'png', 'gif'];
+
+            // Check if the file type is allowed
+            if (in_array($imageFileType, $allowed_types)) {
+                // Move uploaded file to the target directory
+                if (move_uploaded_file($_FILES['image']['tmp_name'], $target_file)) {
+                    $image = $target_file; // Save the file path
+                } else {
+                    echo "Sorry, there was an error uploading the image.";
+                }
+            } else {
+                echo "Only JPG, JPEG, PNG & GIF files are allowed.";
+            }
+        } else {
+            echo "No image uploaded.";
+        }
     }
 }
