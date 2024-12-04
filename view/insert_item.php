609368c75356a0174c73246259debed548a36c9b
Akhtar Hafiz P <akhfizpuli@gmail.com>
2024-12-04 16:07:28 +0700

    file rename

---
+++ view/insert_item.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add new Item</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+<div class="container">
+        <h1>Upload New Item</h1>
+        <form action="" method="post" enctype="multipart/form-data">
+            <label for="name">Item Name:</label><br>
+            <input type="text" name="name" required><br><br>
+
+            <label for="description">Description:</label><br>
+            <textarea name="description" required></textarea><br><br>
+
+            <label for="available">Available Quantity:</label><br>
+            <input type="number" name="available" min="0" required><br><br>
+
+            <label for="image">Upload Image:</label><br>
+            <input type="file" name="image" required><br><br>
+
+            <input type="submit" name="submit" value="Add Item">
+        </form>
+    </div>
+</body>
+</html>
