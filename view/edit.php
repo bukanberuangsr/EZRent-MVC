52f1aae124b11f9133815ee2bc2dcd2058448b46
Akhtar Hafiz P <akhfizpuli@gmail.com>
2024-12-06 14:58:25 +0700

    fix edit item bug

--- view/edit.php
+++ view/edit.php
@@ -9,19 +9,19 @@
 <body>
     <div class="container">
         <h1>Edit Item</h1>
-        <form action="Items&m=update" method="post" enctype="multipart/form-data">
+        <form action="?c=Items&m=update" method="post" enctype="multipart/form-data">
             <label for="name">Item Name:</label><br>
-            <input type="text" name="name" value="<?php echo htmlspecialchars($item['name']); ?>" required><br><br>
+            <input type="text" name="name" value="<?php echo $item->name ?>" required><br><br>
 
             <label for="description">Description:</label><br>
-            <textarea name="description" required><?php echo htmlspecialchars($item['description']); ?></textarea><br><br>
+            <textarea name="description" required><?php echo $item->description ?></textarea><br><br>
 
             <label for="available">Available Quantity:</label><br>
-            <input type="number" name="available" value="<?php echo $item['available']; ?>" min="0" required><br><br>
+            <input type="number" name="available" value="<?php echo $item->available ?>" min="0" required><br><br>
 
             <label for="image">Upload New Image (optional):</label><br>
             <input type="file" name="image"><br><br>
-            <p>Current Image: <br><img src="<?php echo $item['image']; ?>" alt="Current Image" style="width: 100px;"></p>
+            <p>Current Image: <br><img src="<?php echo $item->image ?>" alt="Current Image" style="width: 100px;"></p>
 
             <input type="submit" name="update" value="Update Item">
         </form>
